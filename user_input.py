d5fd8d2ee30bc75abbd7eadcf189d82cbf3a239a
Felixgreat <105022777+Felixgreat@users.noreply.github.com>
2024-03-01 18:17:27 +0100

    first commit

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# INPUT EXERCISE
+# Create a new Python file and name it "user_input.py"
+# Use the input() function to ask the user for their name and store it in a variable called "name".
+# Use the input() function to ask the user for their age and store it in a variable called "age".
+# Use the input() function to ask the user for their location and store it in a variable called "location".
+# Print out a personalized message u
+# sing the user's name, age, and location. For example: "Hello [name], you are [age] years old and live in [location]."
+
+
+name = input("What is your name?")
+age = input("Enter your age")
+location = input("Where do you live?")
+print(f"Hello! {name}, you are {age} years old and live in {location}.")
